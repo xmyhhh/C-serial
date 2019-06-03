18edc8da9e0ea564d609d2ded52397ad764ba25d
xmy <xmycissy@outlook.com>
2019-06-03 21:41:01 +0800

    first

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mood1 = new System.Windows.Forms.CheckBox();
+            this.mood2 = new System.Windows.Forms.CheckBox();
+            this.mood3 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(606, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(96, 106);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(122, 332);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(675, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // mood1
+            // 
+            this.mood1.AutoSize = true;
+            this.mood1.Location = new System.Drawing.Point(606, 267);
+            this.mood1.Name = "mood1";
+            this.mood1.Size = new System.Drawing.Size(36, 16);
+            this.mood1.TabIndex = 4;
+            this.mood1.Text = "鼓";
+            this.mood1.UseVisualStyleBackColor = true;
+            this.mood1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // mood2
+            // 
+            this.mood2.AutoSize = true;
+            this.mood2.Location = new System.Drawing.Point(648, 267);
+            this.mood2.Name = "mood2";
+            this.mood2.Size = new System.Drawing.Size(60, 16);
+            this.mood2.TabIndex = 5;
+            this.mood2.Text = "打地鼠";
+            this.mood2.UseVisualStyleBackColor = true;
+            this.mood2.CheckedChanged += new System.EventHandler(this.mood2_CheckedChanged);
+            // 
+            // mood3
+            // 
+            this.mood3.AutoSize = true;
+            this.mood3.Location = new System.Drawing.Point(714, 267);
+            this.mood3.Name = "mood3";
+            this.mood3.Size = new System.Drawing.Size(48, 16);
+            this.mood3.TabIndex = 6;
+            this.mood3.Text = "心率";
+            this.mood3.UseVisualStyleBackColor = true;
+            this.mood3.CheckedChanged += new System.EventHandler(this.mood3_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(610, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "心率(BMP):";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(606, 249);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "模式：";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.ForeColor = System.Drawing.Color.Coral;
+            this.label4.Location = new System.Drawing.Point(98, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(389, 42);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "超级牛逼智能电鼓APP";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(606, 302);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(48, 16);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "教学";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(96, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 12);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "鼓";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Rhythm1",
+            "Rhythm2",
+            "Rhythm3",
+            "Rhythm4"});
+            this.comboBox1.Location = new System.Drawing.Point(656, 298);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(95, 20);
+            this.comboBox1.TabIndex = 16;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(357, 87);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 12);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "心率(BMP)";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(222, 87);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 12);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "教学提示";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(224, 106);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(129, 332);
+            this.richTextBox2.TabIndex = 10;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(359, 106);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(117, 332);
+            this.richTextBox3.TabIndex = 11;
+            this.richTextBox3.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.mood3);
+            this.Controls.Add(this.mood2);
+            this.Controls.Add(this.mood1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox mood1;
+        private System.Windows.Forms.CheckBox mood2;
+        private System.Windows.Forms.CheckBox mood3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,273 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.DirectX.DirectSound;
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+
+        Microsoft.DirectX.DirectSound.Device dev =
+        new Microsoft.DirectX.DirectSound.Device();
+        bool isPlaying = false;
+        StringBuilder data = new StringBuilder("");
+        StringBuilder datalable = new StringBuilder("");
+        private string dataRead = "";
+        private string TempVal = "0";
+        private string HumidityVal = "0";
+        private string LightVal = "0";
+        StringBuilder Box1 = new StringBuilder("");
+        StringBuilder Box2 = new StringBuilder("");
+        StringBuilder Box3 = new StringBuilder("");
+        SerialPort port =new SerialPort("COM4");
+
+        bool mood_1 = false, mood_2 = false, mood_3 = true,mood_4=false;
+        public delegate void MyInvoke(string str1,int i);
+
+        public Form1()
+        {
+            InitializeComponent();
+            dev.SetCooperativeLevel(this,
+            Microsoft.DirectX.DirectSound.CooperativeLevel.Normal);
+            this.FormClosed += new FormClosedEventHandler(Form1_FormClosed);
+            port.ReceivedBytesThreshold = 1;
+            port.BaudRate = 115200;
+            port.DtrEnable = true;
+            port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+            port.Open();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Post();
+        }
+        private void port_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            dataRead = port.ReadLine();
+            addText(dataRead, 1);
+            addText("\n", 1);
+            DataProcess();
+        }
+
+
+        public static void SetHeaderValue(WebHeaderCollection header, string name, string value)// HTTP协议报文头加入
+
+        {
+            var property = typeof(WebHeaderCollection).GetProperty("InnerCollection", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (property != null)
+            {
+                var collection = property.GetValue(header, null) as NameValueCollection;
+                collection[name] = value;
+            }
+        }
+
+        public void Post()
+        {
+            
+            string url = "http://api.heclouds.com/devices/528166030/datapoints?";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            SetHeaderValue(request.Headers, "api-key", "JWrRnmOzLbxsGazvRKq6qv=43vI=");//设备API地址和 首部参数
+            request.Host = "api.heclouds.com";
+            request.ProtocolVersion = new Version(1, 1);
+            string Cod = "{\"datastreams\":" +
+                "[{\"id\":\"Temp\",\"datapoints\":[{\"value\":\"" + TempVal + "\"}]}," +
+                "{\"id\":\"Humidity\",\"datapoints\":[{\"value\":\"" + HumidityVal + "\"}]}," +
+                "{\"id\":\"Light\",\"datapoints\":[{\"value\":\"" + LightVal + "\"}]}]}";
+            byte[] data = Encoding.UTF8.GetBytes(Cod);
+            request.ContentLength = data.Length;
+            addText("send over \n", 3);
+            using (Stream reqStream = request.GetRequestStream())
+            {
+                reqStream.Write(data, 0, data.Length);
+                reqStream.Close();
+                request.Abort();
+
+            }
+            
+           
+        }//面向OneNet的发送
+
+        private void DataProcess()
+        {
+
+            try
+            {
+
+                if (dataRead.Contains("Temp"))
+                {
+
+
+                    TempVal = dataRead.Replace("Temp=","");
+                    //addText(TempVal, 2);
+                    Post();
+                }
+                else if (dataRead.Contains("Light"))
+                {
+                    LightVal = dataRead.Replace("Light=", "");
+                    //addText(LightVal, 3);
+                    Post();
+                }
+                else if (dataRead.Contains("Humidity"))
+                {
+                    HumidityVal = dataRead.Replace("Humidity=", "");
+                    //addText(HumidityVal, 2);
+                    Post();
+                }
+                else { }
+
+                
+            }
+            catch (Exception e) { }
+        }
+        private void updateText(String updateData,int ID) {
+            
+            MyInvoke mi = new MyInvoke(updateTextMethod);
+            this.Invoke(mi, updateData, ID);
+        }
+        private void addText(String addData, int ID)
+        {
+            MyInvoke mi = new MyInvoke(addTextMethod);
+            this.Invoke(mi,addData,ID);
+        }
+        private void playSound(String url)
+        {
+            try
+            {
+                Microsoft.DirectX.DirectSound.SecondaryBuffer snd =
+                new Microsoft.DirectX.DirectSound.SecondaryBuffer(
+                  url, dev);
+                snd.Play(0, Microsoft.DirectX.DirectSound.BufferPlayFlags.Default);
+            }
+            catch (Exception ex)
+            {
+                ;
+            }
+        }
+       
+        public static void Delay(int milliSecond)
+        {
+            int start = Environment.TickCount;
+            while (Math.Abs(Environment.TickCount - start) < milliSecond)
+            {
+                Application.DoEvents();
+            }
+        }
+        private void updateTextMethod(string text,int ID)
+        {
+            switch (ID) {
+                case 1:{richTextBox1.Text = text;break; }
+                case 2: { richTextBox2.Text = text; break; }
+                case 3: { richTextBox3.Text = text; break; }
+                case 4: { label1.Text = text; break; }
+            }
+        }
+        private void addTextMethod(string text, int ID)
+        {
+            switch (ID)
+            {
+                case 1: { Box1.Append(text); Box1.Append("\n"); richTextBox1.Text = Box1.ToString(); break; }
+                case 2: { Box2.Append(text); Box2.Append("\n"); richTextBox2.Text = Box2.ToString(); break; }
+                case 3: { Box3.Append(text); Box3.Append("\n"); richTextBox3.Text = Box3.ToString(); break; }
+                case 4: { label1.Text = text; break; }
+            }
+        }
+
+        void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (port != null && port.IsOpen)
+            {
+                port.Close();
+              
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            mood_1 = !mood_1;
+        }
+        private void mood2_CheckedChanged(object sender, EventArgs e)
+        {
+            mood_2 = !mood_2;
+            if (mood_2 == true) port.Write("a");
+            else port.Write("b");
+        }
+        private void mood3_CheckedChanged(object sender, EventArgs e)
+        {
+            mood_3 = !mood_3;
+        }
+        private void checkBox1_CheckedChanged_1(object sender, EventArgs e)
+        {
+            mood_4 = !mood_4;
+            if (mood_4 == true) port.Write("c");
+            else port.Write("d");
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+            switch (comboBox1.SelectedItem.ToString()) {
+                case "Rhythm1": {
+                        port.Write("f");
+                        addText("Rhythm1 \n", 1);
+                        break; }
+                case "Rhythm2":
+                    {   
+                        port.Write("g");
+                        addText("Rhythm2 \n", 1);
+                        break;
+                    }
+                case "Rhythm3":
+                    {
+                        port.Write("h");
+                        addText("Rhythm3 \n", 1);
+                        break;
+                    }
+                case "Rhythm4":
+                    {
+                        port.Write("i");
+                        addText("Rhythm4 \n", 1);
+                        break;
+                    }
+                default: break;
+            }
+
+        }
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+}
